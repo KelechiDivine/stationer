5edc29f3a31b5cc301ad064d4927d30e42af526e
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-06-29 12:35:16 +0100

    backend deployed to heroku

--- src/test/java/com/book/repository/book/BookCategoryRepositoryTest.java
+++ src/test/java/com/book/repository/book/BookCategoryRepositoryTest.java
@@ -10,8 +10,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
 @SpringBootTest(classes = RepositoryDataConfig.class)
 @Slf4j
 
@@ -26,12 +24,6 @@ class BookCategoryRepositoryTest {
 	@BeforeEach
 	void setUp() {
 		bookCategory = new BookCategory();
-	}
-	
-	@Test
-	public void testThatWeCanFindBookCategoryById(){
-		BookCategory bookCategory = bookCategoryRepository.findById("60da12fa321bae7f8b8a6d81").orElse(null);
-		assertThat(bookCategory).isNotNull();
 	}
 	
 	@Test
