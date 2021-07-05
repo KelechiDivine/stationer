a736a10a705698caf408999e67be9cdd4bc8bca1
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-07-05 11:28:22 +0100

    new commit

--- src/main/java/com/book/controller/book/BookController.java
+++ src/main/java/com/book/controller/book/BookController.java
@@ -1,6 +1,31 @@
-package com.book.controller.book;
-
-public class BookController {
-
-
-}
+//package com.book.controller.book;
+//
+//import com.book.dto.RegisterBookDto;
+//import com.book.service.exception.BookException;
+//import org.modelmapper.ModelMapper;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//import org.springframework.web.context.request.WebRequest;
+//
+//import javax.validation.Valid;
+//
+//@RestController
+//@RequestMapping("api/books")
+//@CrossOrigin(origins = "http://localhost:3000")
+//
+//public class BookController {
+//
+//	@Autowired
+//	BookServiceImpl bookService;
+//	ModelMapper modelMapper = new ModelMapper();
+//
+//	@PostMapping("/create")
+//
+//	public ResponseEntity<?> registerProduct(@Valid @RequestBody RegisterBookDto registerBookDto, WebRequest webRequest) throws BookException{
+//		if (!bookService.s)
+//	}
+//
+//
+//}
+//
+//
